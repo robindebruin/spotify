54e824c124a007d9bdbce3cd49940a086576c59f
Robin de Bruin <4049120+robindebruin@users.noreply.github.com>
2018-09-24 23:04:33 +0200

    save bearer in session

--- src/atoms/Callback.jsx
+++ src/atoms/Callback.jsx
@@ -1,29 +1,32 @@
 import React, { Component } from 'react';
-import  { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 export class Callback extends Component {
 
   constructor(props) {
-    super(props);    
+    super(props);
     this.callbackObject = props.location.hash && this.hashPropsToObj(props.location.hash);
-    console.log('callbackObject ', this.callbackObject.access_token);    
+    sessionStorage.setItem('BearerToken', `Bearer ${this.callbackObject.access_token}`);
+
+    console.log('BearerToken ', sessionStorage.getItem('BearerToken'));
   }
 
   hashPropsToObj(hash) {
     const callbackWithoutHash = hash.split('#')[1]
-    const callbackArray = callbackWithoutHash.split('&').map( stringObj => stringObj.split('='))
-    return callbackArray.reduce((accumulator, value) => {
-        accumulator[value[0]] = value[1];
-        return accumulator;
-      }, {})
+    const callbackArray = callbackWithoutHash.split('&').map(stringObj => stringObj.split('='))
+    const callbackObject = callbackArray.reduce((accumulator, value) => {
+      accumulator[value[0]] = value[1];
+      return accumulator;
+    }, {})
+    return callbackObject
   }
 
   render() {
-    if (this.callbackObject) return <Redirect to='/'  />
+    if (this.callbackObject) return <Redirect to='/' />
 
     return (
-    <h1>not logged in</h1>
+      <h1>not logged in</h1>
     )
   }
-  
+
 }
